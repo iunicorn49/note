a5d153ed612a4dd61bf9dbf50205b625554c231b
zlk <734775779@qq.com>
2018-01-07 18:19:17 +0800

    修正jsonp

--- 案例/原生实现JSONP/通过promise优化jsonp/main.js
+++ 案例/原生实现JSONP/通过promise优化jsonp/main.js
@@ -3,9 +3,9 @@ let url2 = 'https://api.douban.com/v2/book/1220563'
 let url3 = 'https://api.douban.com/v2/book/1220564'
 
 function getJSONP(options, callback) {
-  let callbackName = options.callbackName ? options.callbackName : 'JSONP'  //  函数名其实无所谓,可以不传,只要有名字就可以
+  let callbackName = options.callbackName ? options.callbackName : 'callbackName'  //  函数名其实无所谓,可以不传,只要有名字就可以
   window[callbackName] = data => {
-    callback(data)
+    callback && callback(data)
     document.body.removeChild(script)
     window[callbackName] = null  //  拿到结果后将这个属性清空留给以后的jsonp请求使用
   }
@@ -17,21 +17,19 @@ function getJSONP(options, callback) {
 
 function changePromise(url) {
   return new Promise((resolve, reject) => {
-    getJSONP({url}, data => {
-      resolve(data)
-    })
+    getJSONP({url}, data => {resolve(data)})
   })
 }
 
 //  经过promise封装实现链式调用, 并保证顺序
-changePromise(url1, 'js1')
+changePromise(url1)
   .then(data => {
     console.log(data)
-    return changePromise(url2, 'js2')
+    return changePromise(url2)
   })
   .then(data => {
     console.log(data)
-    return changePromise(url3, 'js3')
+    return changePromise(url3)
   })
   .then(data => {
     console.log(data)
