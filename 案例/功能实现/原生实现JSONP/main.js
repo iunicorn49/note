95537d675a9fea1272c1b2db984e17a5d6799588
zlk <734775779@qq.com>
2018-01-11 17:18:32 +0800

    优化了jsonp

--- 案例/功能实现/原生实现JSONP/main.js
+++ 案例/功能实现/原生实现JSONP/main.js
@@ -1,25 +1,47 @@
-let url = 'https://api.douban.com/v2/book/1220562'
+let url1 = 'https://api.douban.com/v2/book/1220562'
 let url2 = 'https://api.douban.com/v2/book/1220563'
 let url3 = 'https://api.douban.com/v2/book/1220564'
 
-function getJSONP(url, callbackName, callback) {
-  window[callbackName] = data => {
-    callback(data)
-    document.body.removeChild(script)
-    window[callbackName] = null
+function jsonp(options) {
+  let callbackName = options.callbackName ? options.callbackName : 'callbackName' // 设置回调函数名称(可选),如果不通过Promise进行同步请求必须设置
+  let src = `${options.url}?callback=${callbackName}` // 设置script标签的src地址
+  if (options.time) { // 可以选择设置请求超时时间,用来监测jsonp的错误
+    var timer = setTimeout(() => { // let会导致块级作用域,为了后续能监测到timer,这里用var
+      document.body.removeChild(script)
+      window[callbackName] = null
+      options.error && options.error()
+    }, options.time)
   }
-  let src = `${url}?callback=${callbackName}`
   let script = document.createElement('script')
   script.src = src
   document.body.appendChild(script)
+  window[callbackName] = data => {
+    if (options.time) clearTimeout(timer)
+    document.body.removeChild(script)
+    window[callbackName] = null
+    options.success && options.success(data)
+  }
 }
+
 //  异步请求, 返回的顺序是不固定的
-getJSONP(url, 'js1', function (data) {
-  console.log(1, data)
+jsonp({
+  url: url1,
+  time: 3000,
+  callbackName: 'jsonp1',
+  success(data) {console.log(1,data)},
+  error() {console.log(1,'获取失败')}
 })
-getJSONP(url2, 'js2', function (data) {
-  console.log(2, data)
+jsonp({
+  url: url2,
+  time: 3000,
+  callbackName: 'jsonp2',
+  success(data) {console.log(2,data)},
+  error() {console.log(2,'获取失败')}
 })
-getJSONP(url3, 'js3', function (data) {
-  console.log(3, data)
+jsonp({
+  url: url3,
+  time: 3000,
+  callbackName: 'jsonp3',
+  success(data) {console.log(3,data)},
+  error() {console.log(3,'获取失败')}
 })
--- 案例/功能实现/原生实现JSONP/通过promise优化jsonp/main.js
+++ 案例/功能实现/原生实现JSONP/通过promise优化jsonp/main.js
@@ -1,36 +1,57 @@
-let url1 = 'https://api.douban.com/v2/book/1220562'
-let url2 = 'https://api.douban.com/v2/book/1220563'
-let url3 = 'https://api.douban.com/v2/book/1220564'
-
-function getJSONP(options, callback) {
-  let callbackName = options.callbackName ? options.callbackName : 'callbackName'  //  函数名其实无所谓,可以不传,只要有名字就可以
-  window[callbackName] = data => {
-    callback && callback(data)
-    document.body.removeChild(script)
-    window[callbackName] = null  //  拿到结果后将这个属性清空留给以后的jsonp请求使用
+function jsonp(options) {
+  let callbackName = options.callbackName ? options.callbackName : 'callbackName' // 设置回调函数名称(可选),如果不通过Promise进行同步请求必须设置
+  let src = `${options.url}?callback=${callbackName}` // 设置script标签的src地址
+  if (options.time) { // 可以选择设置请求超时时间,用来监测jsonp的错误
+    var timer = setTimeout(() => { // let会导致块级作用域,为了后续能监测到timer,这里用var
+      document.body.removeChild(script)
+      window[callbackName] = null
+      options.error && options.error()
+    }, options.time)
   }
-  let src = `${options.url}?callback=${callbackName}`
   let script = document.createElement('script')
   script.src = src
   document.body.appendChild(script)
+  window[callbackName] = data => {
+    if (options.time) clearTimeout(timer)
+    document.body.removeChild(script)
+    window[callbackName] = null
+    options.success && options.success(data)
+  }
 }
 
-function changePromise(url) {
+// 通过promis封装可以进行同步请求以及链式调用
+function syncJsonp(url) {
   return new Promise((resolve, reject) => {
-    getJSONP({url}, data => {resolve(data)})
+    jsonp({
+      url,
+      time: 3000,
+      success(data) {resolve(data)},
+      error() {reject('获取失败')}
+    })
   })
 }
 
-//  经过promise封装实现链式调用, 并保证顺序
-changePromise(url1)
+let url1 = 'https://api.douban.com/v2/book/1220562'
+let url2 = 'https://api.douban.com/v2/book/122056' // 这是一个错误的地址,获取不到数据
+let url3 = 'https://api.douban.com/v2/book/1220564'
+
+syncJsonp(url1)
   .then(data => {
-    console.log(data)
-    return changePromise(url2)
+    console.log(1,data)
+    return syncJsonp(url2)
+  }, err => {
+    console.log(1,err)
+    return syncJsonp(url2)
   })
   .then(data => {
-    console.log(data)
-    return changePromise(url3)
+    console.log(2,data)
+    return syncJsonp(url3)
+  }, err => {
+    console.log(2,err)
+    return syncJsonp(url3)
   })
   .then(data => {
-    console.log(data)
+    console.log(3,data)
+  }, err => {
+    console.log(3,err)
   })
